f7acbd0b9f186e8dc0186862760804bd3de0d0f8
Aanya Kungwani <aanyak820@gmail.com>
2025-07-10 10:25:52 -0400

    Added second view!

---
+++ Navigation/SecondView.swift
@@ -0,0 +1,18 @@
+//
+//  SecondView.swift
+//  Navigation
+//
+//  Created by aanya kungwani on 7/10/25.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        Text("This is the second view, welcome!")
+    }
+}
+
+#Preview {
+    SecondView()
+}
